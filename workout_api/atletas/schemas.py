319934dd544d2ac8714860c70186368cba1ef0ff
palermoc <pedropalermo97@gmail.com>
2024-04-11 12:34:47 -0300

    Desc-3/Add settings and started atletas

--- workout_api/atletas/schemas.py
+++ workout_api/atletas/schemas.py
@@ -1,12 +1,17 @@
 from typing import Annotated
 from pydantic import Field, PositiveFloat
 
-from workout_api.contrib.schemas import BaseSchema
+from workout_api.contrib.schemas import BaseSchema, OutMixin
 
-class Atelta(BaseSchema):
+class Atleta(BaseSchema):
     nome: Annotated[str, Field(description='Nome do atleta', examples ='alex', max_length=50)]
     cpf: Annotated[str, Field(description='Cpf do atleta', examples ='16489301711', max_length=11)]
     idade: Annotated[int, Field(description='Idade do atleta', examples ='24')]
     peso: Annotated[PositiveFloat, Field(description='Peso do atleta', examples ='72.1')]
     altura: Annotated[PositiveFloat, Field(description='Altura do atleta', examples ='1.70')]
     sexo: Annotated[str, Field(description='Sexo do atleta', examples ='M', max_length=1)]
+
+class AtletaIn(Atleta):
+    pass 
+class AtletaOut(Atleta, OutMixin):
+    pass
